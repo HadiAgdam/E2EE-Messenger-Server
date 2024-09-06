754dcddb501db25f0e0d52a5a78653402a686658
HadiAgdam <hadiagdamdeveloper@gmail.com>
2024-09-06 17:21:39 +0330

    created get and set message function. But not implemented

---
+++ data.py
@@ -0,0 +1,11 @@
+from models import Message
+
+
+def new_message(message: Message):
+    # TODO insert new message to database
+    pass
+
+
+def get_messages_by_time_stamp(p_key: str, time_stamp: int) -> list:
+    # TODO get messages from database where public key matches and time stamp of message is bigger than time_stamp
+    pass
