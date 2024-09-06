6c3302606f10e00b027960caa3da59f2a0e27e04
HadiAgdam <hadiagdamdeveloper@gmail.com>
2024-09-06 22:36:58 +0330

    added get_message_by_id and new_message functions

--- data.py
+++ data.py
@@ -1,11 +1,21 @@
+import sqlite3
 from models import Message
+
+conn = sqlite3.connect('e2ee_messenger_server.db')
+cursor = conn.cursor()
 
 
 def new_message(message: Message):
-    # TODO insert new message to database
-    pass
+    cursor.execute(
+        "INSERT INTO messages (recipient_public_key, encrypted_key, encrypted_message) VALUES (?, ?, ?)",
+        (message.receiver, message.key, message.message)
+    )
+    conn.commit()
 
 
 def get_messages_by_id(p_key: str, last_message_id: int) -> list:
-    # TODO get messages from database where public key matches and time stamp of message is bigger than time_stamp
-    pass
+    cursor.execute(
+        "SELECT * FROM messages WHERE recipient_public_key = ? AND message_id > ?",
+        (p_key, last_message_id)
+    )
+    return cursor.fetchall()
