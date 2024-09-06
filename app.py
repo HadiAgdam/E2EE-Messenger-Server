0fce0e9957385f4f36429554d1f33de569033072
HadiAgdam <hadiagdamdeveloper@gmail.com>
2024-09-06 21:26:54 +0330

    replaced time_stamp with last_message_id

--- app.py
+++ app.py
@@ -13,11 +13,11 @@ INVALID_ARGS = 400
 def _new_message():
     data = request.get_json()
 
-    if 'key' in data and 'message' in data and 'time_stamp' in data:
+    if 'receiver' in data and 'key' in data and 'message' in data:
         message = Message(
-            key=data.get("json"),
-            message=data.get("json"),
-            time_stamp=data.get("time_stamp")
+            receiver=data.get("receiver"),
+            key=data.get("key"),
+            message=data.get("message"),
         )
         new_message(message)
         return jsonify({
@@ -29,15 +29,16 @@ def _new_message():
         }), INVALID_REQUEST_DATA
 
 
-@app.route("/api/get_updates", methods=["GET"])
+@app.route("/api/get_message", methods=["GET"])
 def _get_updates():
     public_key = request.args.get("p_key")
 
     if not public_key:
         return jsonify({"error:": "invalid arguments"}), INVALID_ARGS
-    last_time_stamp = request.args.get("time_stamp", 0)
 
-    messages = get_messages_by_time_stamp(public_key, last_time_stamp)
+    last_id = request.args.get("message_id", 0)
+
+    messages = get_messages_by_time_stamp(public_key, last_id)
 
     return jsonify(messages), SUCCESS
 
