e3c5a887c0694bd1412a851eafcd561fafbf704d
HadiAgdam <hadiagdamdeveloper@gmail.com>
2024-09-06 21:28:03 +0330

    replaced time_stamp with last_message_id

--- models.py
+++ models.py
@@ -1,10 +1,18 @@
+from time import time
+
+
 class Message:
     def __init__(
             self,
+            receiver: str,
             key: str,
             message: str,
-            time_stamp: int
+            time_stamp: int = -1
     ):
+        self.receiver = receiver
         self.key = key
         self.message = message
-        self.time_stamp = time_stamp
+        if time_stamp == -1:
+            self.time_stamp = time()
+        else:
+            self.time_stamp = time_stamp
